9c3b0f96db2de80930678d4a0f78ddfc31c5b081
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-10-09 18:53:52 +0500

    Task4V14

---
+++ Tyuiu.KochetovKO.Sprint2.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KochetovKO.Sprint2.Task4.V14.Lib;
+namespace Tyuiu.KochetovKO.Sprint2.Task4.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 10.5;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 15;
+            double res = ds.Calculate(x, y);
+            double wait = 11;
+            Assert.AreEqual(wait, res); 
+        }
+
+    }
+}
---
+++ Tyuiu.KochetovKO.Sprint2.Task4.V14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KochetovKO.Sprint2.Task4.V14.Lib;
+namespace Tyuiu.KochetovKO.Sprint2.Task4.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("ИСХОДНЫЕ ДАННЫЕ :                                                               ");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("Введите значение x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ :                                                                     ");
+            Console.WriteLine("********************************************************************************");
+
+            double res = ds.Calculate(x, y);
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        
+        
+        }
+    }
+}
