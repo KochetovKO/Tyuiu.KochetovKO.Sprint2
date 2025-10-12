364093f737770d393712cb89f85b2040df0aa663
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-10-12 17:43:11 +0500

    Task5V11

---
+++ Tyuiu.KochetovKO.Sprint2.Task5.V11.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.SlokvaGA.Sprint2.Task5.V13.Lib
+{
+    public class DataService : ISprint2Task5V11
+    {
+        public string FindDateOfNextDay(int g, int m, int n)
+        {
+            
+        }
+    }
+}
---
+++ Tyuiu.KochetovKO.Sprint2.Task5.V11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KochetovKO.Sprint2.Task5.V11.Lib;
+using static System.Net.Mime.MediaTypeNames;
+namespace Tyuiu.KochetovKO.Sprint2.Task5.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("ИСХОДНЫЕ ДАННЫЕ :                                                               ");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("Введите год :");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите месяц :");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число :");
+            int date = Convert.ToInt32(Console.ReadLine());
+
+            string res = ds.FindDateOfNextDay(year, month, date);
+
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ :                                                                     ");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("Следующий день: " + res);
+            Console.ReadKey();
+
+
+        }
+    }
+}
