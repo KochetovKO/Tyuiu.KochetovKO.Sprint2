bee07bbb9f60fb830cfc0775641ab4b98cd1d115
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-10-12 18:12:44 +0500

    Task5V11

--- Tyuiu.KochetovKO.Sprint2.Task5.V11/Program.cs
+++ Tyuiu.KochetovKO.Sprint2.Task5.V11/Program.cs
@@ -27,7 +27,7 @@ namespace Tyuiu.KochetovKO.Sprint2.Task5.V11
             int date = Convert.ToInt32(Console.ReadLine());
 
 
-            string res = ds.FindDateOfNextDay(year, month, date); date.ToString("dd.MM.yyyy");
+            string res = ds.FindDateOfNextDay(year, month, date); date.ToString("dd.MM.yyyy");  
 
             DateTime res1 = DateTime.Parse(res); 
             string fd = res1.ToString("dd.MM.yyyy");
